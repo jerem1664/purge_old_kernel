77858cbea4f298801aa6825dc072ad6c8ab48ba8
Jeremy Barron <jbarron@ippon.fr>
2017-12-12 17:41:27 +0100

    rename c est mieux

--- clean_kernel.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# clean installed kernel, except the current one.
-
-
-CURRENT=$(uname -r | /bin/grep -oE '^[0-9]+.[0-9]+.[0-9]+\-[0-9]+')
-
-/usr/bin/dpkg -l | /bin/grep "ii  linux-\(headers\|image\)" | /bin/grep -v $CURRENT | /usr/bin/awk '{print $2}' | /usr/bin/xargs /usr/bin/apt-get purge -y
